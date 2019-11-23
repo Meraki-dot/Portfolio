353f64a155c513761bf0a3490f1007789788d90a
Meraki-dot <52216004+Meraki-dot@users.noreply.github.com>
2019-11-22 16:27:33 -0800

    update

---
+++ src/components/BrowserNotes/BrowserNotes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Browser from '@components/Browser';
+
+const BrowserNotes = () => {
+  return (
+    <div className="browser-notes">
+      <Browser except firefox chrome mobile>
+        <span><b>Meraki</b>: (.v) to do something with soul, creativity, or love</span>
+      </Browser>
+      <Browser only firefox>
+        <span><b>Meraki</b>: (.v) to do something with soul, creativity, or love</span>
+        <br></br>
+        <br></br>
+        <span>Magic background is disabled in FireFox. Try in <b>Chrome!</b></span>
+      </Browser>
+    </div>
+  );
+};
+
+export default BrowserNotes;
