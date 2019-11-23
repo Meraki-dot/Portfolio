0df6ec0230fe92d42b6c1206a552e0a0f8e5e7f3
Meraki-dot <52216004+Meraki-dot@users.noreply.github.com>
2019-11-22 16:03:42 -0800

    firefox build fix

--- portfolio/src/components/BrowserNotes/BrowserNotes.js
+++ portfolio/src/components/BrowserNotes/BrowserNotes.js
@@ -5,10 +5,12 @@ const BrowserNotes = () => {
   return (
     <div className="browser-notes">
       <Browser except firefox chrome mobile>
-        <span><b>Meraki</b>: (.v) to do something with soul, creativity, or love; to put something of yourself in your work</span>
+        <span><b>Meraki</b>: (.v) to do something with soul, creativity, or love</span>
       </Browser>
       <Browser only firefox>
-        <span><b>Meraki</b>: (.v) to do something with soul, creativity, or love; to put something of yourself in your work</span>
+        <span><b>Meraki</b>: (.v) to do something with soul, creativity, or love</span>
+        <br></br>
+        <br></br>
         <span>Magic background is disabled in FireFox. Try in <b>Chrome!</b></span>
       </Browser>
     </div>
